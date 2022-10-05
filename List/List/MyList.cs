46cbae7b6fafb97278279e8fffd0482a3ec1e063
Vladyslav Drushliak <vladislav.drushliak@gmail.com>
2022-10-05 09:18:00 +0300

    Adding items to the list

---
+++ List/List/MyList.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    public class MyList<Type> : IMyListTemplate<Type>
+    {
+        private int _initialSize = 4;
+        private int _index = 0;
+        private Type[] _array;
+
+        public MyList()
+        {
+            _array = new Type[_initialSize];
+        }
+
+        public MyList(params Type[] values)
+            : this()
+        {
+            Add(values);
+        }
+
+        public int GetSize => _array.Length;
+
+        public void Add(params Type[] values)
+        {
+            if (values.Length > _array.Length - _index)
+            {
+                var index = 0;
+                _initialSize *= 2;
+
+                Type[] backupArray = _array;
+                _array = new Type[_initialSize];
+
+                foreach (var value in backupArray)
+                {
+                    _array[index++] = value;
+                }
+            }
+
+            foreach (var value in values)
+            {
+                _array[_index++] = value;
+            }
+        }
+
+        public void Show()
+        {
+            foreach (var value in _array)
+            {
+                Console.Write($"{value} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
--- List/List/Program.cs
+++ List/List/Program.cs
@@ -6,6 +6,11 @@ namespace List
     {
         public static void Main(string[] args)
         {
+            var list1 = new MyList<int>(2, 5, 8, 4);
+            list1.Add(3);
+            list1.Show();
+
+            Console.WriteLine($"size: {list1.GetSize}");
         }
     }
 }
