613ff6ed74d1d214e2e39b0b34417f3506c701da
Vladyslav Drushliak <vladislav.drushliak@gmail.com>
2022-10-06 17:15:41 +0300

    Read Only

--- List/List/Program.cs
+++ List/List/Program.cs
@@ -7,6 +7,18 @@ namespace List
     {
         public static void Main(string[] args)
         {
+            var users = GetUsers();
+
+            foreach (var user in users)
+            {
+                Console.Write($"{user.Name} ");
+            }
+
+            Console.WriteLine($"count: {users.Count}");
+        }
+
+        public static IReadOnlyCollection<User> GetUsers()
+        {
             var user1 = new User("Tom");
             var user2 = new User("Alice");
             var user3 = new User("Li");
@@ -14,26 +26,13 @@ namespace List
             var user5 = new User("Bob");
 
             var users = new MyList<User>(user1, user2, user3, user5);
-            Show(users);
 
             users.Remove(user5);
             users.Remove(user1);
-            Show(users);
 
             users.Add(user3, user3, user3);
-            Show(users);
-        }
 
-        public static void Show(MyList<User> list)
-        {
-            foreach (var item in list)
-            {
-                Console.Write($"{item.Name} ");
-            }
-
-            Console.WriteLine();
-            Console.WriteLine($"count: {list.Count}");
-            Console.WriteLine($"size: {list.Size}");
+            return users;
         }
     }
 }
